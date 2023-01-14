bec1f75edbf2e24a9f90e9147d328b7e9615acdd
JeanG1914 <jeanpaulgzea@gmail.com>
2023-01-14 01:12:39 +0000

    Start

---
+++ src/js/component/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar(_props) {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        LandingPage
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
